59a90e7df2f0946c91a4fe9d9d36a98d2e2e6a36
nawanernandess <nawanernandess@gmail.com>
2022-06-27 17:44:15 -0300

    adicionando js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Ativar links de navegação
+const links = document.querySelectorAll(".header-menu a")
+
+function ativarLink(link){
+    const url = location.href
+    const href = link.href
+
+    if(url.includes(href)){
+        link.classList.add('ativo')
+    }
+}
+links.forEach(ativarLink)
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro)
+    
+    if(elemento){
+        elemento.checked = true
+    }
+}
+
+parametros.forEach(ativarProduto)
